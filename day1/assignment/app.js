d78421813af1d27c5143776dbaf249626b004a4c
wfoody <foodyw@gmail.com>
2020-10-26 18:29:40 -0400

    fixing post display, adding ability to delete

--- day1/assignment/app.js
+++ day1/assignment/app.js
@@ -21,27 +21,40 @@ app.get('/', (req, res) => {
     
     db.any('SELECT post_id, title, body, date_created, date_updated, is_published FROM posts;')
     .then(posts => {
-       // console.log("FETCHED POSTS")
+        console.log("FETCHED POSTS")
+        console.log(posts)
         res.render('index',{posts: posts})
     })
+
 })
 
 app.post('/add-album', (req, res) => {
 
     const albumName = req.body.albumName 
     const albumReview = req.body.albumReview 
-    const date_created = req.body.date_created
-    const is_published = req.body.is_published
+    // const date_created = req.body.date_created
+    // const is_published = req.body.is_published
     
     /* INSERT INTO users(first_name, last_name, age) VALUES('George', 'Doe', 45);  */
     
-    db.none('INSERT INTO posts(title, body, date_created, is_published) VALUES($1,$2,$3,$4)',[albumName, albumReview, date_created, is_published])
+    db.none('INSERT INTO posts(title, body) VALUES($1,$2)',[albumName, albumReview])
     .then(() => {
        // do something after inserted successfully! 
        res.redirect('/')
     })
     
- })  
+ }) 
+ 
+ app.post('/delete-album',(req,res) => {
+
+    const post_id = req.body.post_id 
+
+    db.none('DELETE FROM posts WHERE post_id = $1;',[post_id])
+        .then(() => {
+            res.redirect('/')
+        })
+
+})
 
 app.listen(3000, () => {
     console.log('Server is running...')
